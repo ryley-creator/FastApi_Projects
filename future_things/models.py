29b11a8d1dfe1368c33d62952d3a945820378052
Shodiyor <nodirbotir788@gmail.com>
2025-12-26 04:09:14 -0800

    Delete future_things directory

--- future_things/models.py
+++
@@ -1,9 +0,0 @@
-from database import Base
-from sqlalchemy import Column,Integer,String
-
-class Future(Base):
-    __tablename__ = 'items'
-    
-    id = Column(Integer,primary_key=True,nullable=False,autoincrement=True)
-    product_name = Column(String(50),nullable=False)
-    description = Column(String(500),nullable=False)
